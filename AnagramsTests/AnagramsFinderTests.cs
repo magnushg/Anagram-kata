22aba2f0da99e17e6a3c1d91a62b37777dac720e
Magnus Green <magnus.green@bouvet.no>
2012-03-14 00:14:36 +0100

    Initial commit with starter tests

---
+++ AnagramsTests/AnagramsFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramsTests
+{
+    [TestClass]
+    public class AnagramsFinderTests
+    {
+        [TestMethod]
+        public void Should_Not_Find_Anagrams_In_Empty_Input()
+        {
+        }
+
+        [TestMethod]
+        public void Should_Find_One_Anagram_Group_When_Input_Contains_Two_Words_Are_Anagrams()
+        {
+        }
+
+        [TestMethod]
+        public void Should_Find_Multiple_Anagram_Groups_When_Input_Contains_Serveral_Words_That_Are_Anagrams()
+        {
+        }
+    }
+}
